6cabeb4287ddf683546b8086d5fd45b60100e19f
quyixiao <2621048238@qq.com>
2019-06-19 12:41:56 +0800

    提交测试

---
+++ src/main/java/com/springmvc/MyServlet.java
@@ -0,0 +1,41 @@
+package com.springmvc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+
+    public void init() {
+        System.out.println("this is init method ");
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        handleLogic(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+        handleLogic(request, response);
+    }
+
+
+    private void handleLogic(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("handle myLogic");
+        ServletContext sc = getServletContext();
+        RequestDispatcher rd = null;
+        rd = sc.getRequestDispatcher("/index.jsp");
+
+        try {
+            rd.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}

f983855137d5aba7f22cfa71f87b5445ebd82bf3
quyixiao <2621048238@qq.com>
2019-06-20 18:23:12 +0800

    提交测试

--- src/main/java/com/springmvc/ExceptionHandler.java
+++ src/main/java/com/springmvc/ExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.springmvc;
 
-import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,9 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 public class ExceptionHandler implements HandlerExceptionResolver {
     @Override
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        request.setAttribute("exception",ex.toString());
-        request.setAttribute("exceptionStack",ex);
+        request.setAttribute("exception", ex.toString());
+        request.setAttribute("exceptionStack", ex);
         System.out.println(ex.toString());
         return new ModelAndView("/error/exception");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("test add msm");
+        System.out.println("测试我们测试他们东西 void main();");
+        System.out.println("void main() 测试他们东西是这样的");
     }
 }
